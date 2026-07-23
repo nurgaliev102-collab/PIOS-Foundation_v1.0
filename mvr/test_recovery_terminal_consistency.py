281ca93cc4cf00eedd85dfb436212bfc1cfaa43f
nurgaliev102-collab <nurgaliev102@gmail.com>
2026-07-23 09:31:12 +0300

    Milestone 12: align legacy recovery test with constructor admission gate

--- mvr/test_recovery_terminal_consistency.py
+++ mvr/test_recovery_terminal_consistency.py
@@ -43,13 +43,13 @@ class RecoveryTerminalConsistencyTests(unittest.TestCase):
         second = self.s.propose_fenced(1, 11, [10, 11])
         self.assertEqual(2, second.fence_token)
 
-    def test_committed_terminal_without_assignment_fails_closed(self):
+    def test_committed_terminal_without_assignment_fails_closed_at_admission(self):
         fp = self.s.propose_fenced(1, 10, [10])
         with self.s.ledger.conn:
             self.s.ledger.conn.execute("INSERT INTO proposal_terminal(proposal_id,terminal_type) VALUES(?,'COMMITTED')", (fp.proposal.id,))
-        self.restart()
-        with self.assertRaisesRegex(DispatchConflict, 'COMMITTED_WITHOUT_ASSIGNMENT'):
-            self.s.accept_fenced(fp.proposal.id, fp.fence_token, 'K1')
+        self.s.close()
+        with self.assertRaisesRegex(DispatchConflict, 'TERMINAL_EVENT_MISMATCH|TERMINAL_ASSIGNMENT_MISMATCH'):
+            FencedDispatchService(self.path)
 
     def test_terminal_event_disagreement_is_detectable(self):
         fp = self.s.propose_fenced(1, 10, [10])
@@ -60,7 +60,7 @@ class RecoveryTerminalConsistencyTests(unittest.TestCase):
             self.s.validate_recovery_consistency()
 
     def test_fence_generation_mismatch_is_detectable(self):
-        fp = self.s.propose_fenced(1, 10, [10])
+        self.s.propose_fenced(1, 10, [10])
         with self.s.ledger.conn:
             self.s.ledger.conn.execute("UPDATE order_fence SET current_token=99 WHERE order_id=1")
         with self.assertRaises(DispatchConflict):
